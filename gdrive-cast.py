0fa93fb5828cb033f3677c8b3da9eb581c83a346
Igor Rogov <igor.rogov.35@gmail.com>
2025-08-02 12:05:33 +1000

    experimenting with YouTube API

--- gdrive-cast.py
+++ gdrive-cast.py
@@ -1,3 +1,6 @@
+import sys
+
+from googleapiclient import discovery
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 from pydrive2.files import GoogleDriveFile
@@ -10,6 +13,16 @@ def auth() -> GoogleAuth:
     gauth = GoogleAuth()
 
     gauth.LoadCredentialsFile("mycreds.txt")
+
+    gauth.auth_params = {
+        'access_type': 'offline',
+        'prompt': 'consent'
+    }
+    gauth.settings['oauth_scope'] = [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/youtube.readonly'
+    ]
+
     if gauth.credentials is None:
         gauth.LocalWebserverAuth()
     elif gauth.access_token_expired:
@@ -21,18 +34,19 @@ def auth() -> GoogleAuth:
     return gauth
 
 
-def get_root_folder(drive) -> GoogleDriveFile:
+def get_or_create_folder(drive, name, parent_folder_id) -> GoogleDriveFile:
     roots = drive.ListFile({
-        'q': f"title='{ROOT_FOLDER}' and 'root' in parents and trashed=false and mimeType='{FOLDER_TYPE}'"
+        'q': f"title='{name}' and '{parent_folder_id}' in parents and trashed=false and mimeType='{FOLDER_TYPE}'"
     }).GetList()
 
     if roots:
         return roots[0]
 
     # If the list is empty, the folder doesn't exist.
-    print(f"Folder '{ROOT_FOLDER}' not found. Creating a new one...")
+    print(f"Folder '{name}' not found. Creating a new one...")
     folder_metadata = {
-        'title': ROOT_FOLDER,
+        'title': name,
+        'parents': [{'id': parent_folder_id}],
         'mimeType': FOLDER_TYPE
     }
     folder = drive.CreateFile(folder_metadata)
@@ -41,20 +55,56 @@ def get_root_folder(drive) -> GoogleDriveFile:
     return folder
 
 
-drive = GoogleDrive(auth())
-root = get_root_folder(drive)
+def upload_file(file_path, folder_id):
+    file_to_upload = drive.CreateFile({'parents': [{'id': folder_id}]})
+    file_to_upload.SetContentFile(file_path)
+    file_to_upload.Upload()
+
+    print(f"Uploaded file: `{file_to_upload}`")
+    print(f"Uploaded file (direct link): https://drive.usercontent.google.com/download?export=download&confirm=t&id=`{file_to_upload['id']}`")
+
+    # add "Anyone with link" permission
+    file_to_upload.InsertPermission({
+        'type': 'anyone',
+        'value': 'anyone',
+        'role': 'reader'}
+    )
+
+
+class YouTubeVideo:
+
+    def __init__(self, youtube, id):
+        response = youtube.videos().list(part='snippet,contentDetails', id=id).execute()
+        snippet = response['items'][0]['snippet']
+
+        self.title = snippet['title']
+        self.description = snippet['description']
+        self.published = snippet['publishedAt']
+        self.thumbnail_url = snippet['thumbnails']['standard']['url']
+
+        self.channel_id = snippet['channelId']
+        self.channel_title = snippet['channelTitle']
+
+
+# authenticate and init services
+gauth = auth()
+drive = GoogleDrive(gauth)
+youtube = discovery.build('youtube', 'v3', credentials=gauth.credentials)
+
+root = get_or_create_folder(drive, ROOT_FOLDER, 'root')
 print('root: %s' % root)
 
-# upload file
-file_to_upload = drive.CreateFile({'parents': [{'id': root['id']}]})
-file_to_upload.SetContentFile('sample.mp3')
-file_to_upload.Upload()
-print(f"Uploaded file: `{file_to_upload}`")
+video_id = sys.argv[1]
+video = YouTubeVideo(youtube=youtube, id=video_id)
 
-# add "Anyone with link" permission
-file_to_upload.InsertPermission({
-    'type': 'anyone',
-    'value': 'anyone',
-    'role': 'reader'}
-)
-print(f"Uploaded file (direct link): https://drive.usercontent.google.com/download?export=download&confirm=t&id=`{file_to_upload['id']}`")
+print("--- Video Details ---")
+print(f"Title: {video.title}")
+print(f"Published at: {video.published}")
+print(f"Thumbnails: {video.thumbnail_url}")
+print(f"Channel Name: {video.channel_title}")
+print(f"Channel ID: {video.channel_id}")
+print("\n--- Description ---")
+print(video.description)
+
+channel_folder = get_or_create_folder(drive, video.channel_id, root['id'])
+print('channel_folder: %s' % root)
